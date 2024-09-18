9f8e16a733ba5e4ebde3941ce69a80d1f26acf19
Lluc Llull <lluc@clicktotravel.es>
2024-09-18 13:32:54 +0200

    add basic app structure

---
+++ src/app/shared/components/rating-stars/rating-stars.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rating-stars',
+  standalone: true,
+  imports: [],
+  templateUrl: './rating-stars.component.html',
+  styleUrl: './rating-stars.component.scss'
+})
+export class RatingStarsComponent {
+
+}
