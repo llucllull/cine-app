cfc24efaa89fb35294cd761e64ab1dfbbe8b55dd
Lluc Llull <lluc@clicktotravel.es>
2024-09-18 15:34:40 +0200

    add tv series + home page

---
+++ src/app/features/tv-list/tv-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tv-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './tv-list.component.html',
+  styleUrl: './tv-list.component.scss'
+})
+export class TvListComponent {
+
+}
